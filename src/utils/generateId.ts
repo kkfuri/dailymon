ca640229763ee36a1ae2c9256a89f7aa8379aee7
kkfuri <kkfuri@gmail.com>
2020-11-17 00:39:45 -0300

    fix: update generateId function

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,7 +1,7 @@
 export function generateId() {
   const date = new Date()
   const generatedNumber =
-    date.getFullYear() + date.getUTCDay() + date.getUTCMonth()
+    date.getFullYear() + date.getUTCDate() + date.getUTCMonth()
   const id = generatedNumber > 800 ? generatedNumber / 4 : generatedNumber
 
   return Math.round(id)
