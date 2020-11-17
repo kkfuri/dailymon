fbabe591c3c44664bc7b1bc448a3a8631880ec39
kkfuri <kkfuri@gmail.com>
2020-11-17 01:00:40 -0300

    feat: attempt to add static homepage

--- src/utils/generateId.ts
+++
@@ -1,8 +0,0 @@
-export function generateId() {
-  const date = new Date()
-  const generatedNumber =
-    date.getFullYear() + date.getUTCDate() + date.getUTCMonth()
-  const id = generatedNumber > 800 ? generatedNumber / 4 : generatedNumber
-
-  return Math.round(id)
-}
