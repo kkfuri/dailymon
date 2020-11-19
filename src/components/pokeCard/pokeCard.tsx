608ee2fda2e79698b63436e27fb4513ce73f3988
kkfuri <kkfuri@gmail.com>
2020-11-19 19:56:32 -0300

    feat: improve styling

--- src/components/pokeCard/pokeCard.tsx
+++ src/components/pokeCard/pokeCard.tsx
@@ -1,5 +1,13 @@
 import Image from 'next/image'
-import { Box, Flex, Heading, Skeleton, Spinner, Stack } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Heading,
+  LinkBox,
+  Skeleton,
+  Spinner,
+  Stack,
+} from '@chakra-ui/react'
 import { useQuery } from 'react-query'
 import chroma from 'chroma-js'
 import Axios from 'axios'
@@ -31,8 +39,6 @@ export const Pokecard = ({ url, name }) => {
       ? chroma.average(formattedTypes.map((i) => colorByType[i]))
       : mainColor
 
-  console.log(random(10, 20))
-
   return (
     <Link href={`/${id}`} passHref>
       <Box
@@ -41,96 +47,95 @@ export const Pokecard = ({ url, name }) => {
         role="group"
         h={130}
         w={340}
-        p={1}
-        cursor="pointer"
-        bg={chroma(avgColor).brighten().alpha(0.2).css()}
         borderRadius={12}
-        transition="padding .2s"
-        _hover={{ p: 0 }}
+        backgroundImage={`url(${
+          sprites?.other?.['official-artwork']?.front_default
+        }), linear-gradient(150deg, ${chroma(avgColor)
+          .brighten(0.8)
+          .css()}, ${mainColor})`}
+        backgroundRepeat="no-repeat"
+        backgroundPosition="110% 30%,center"
+        backgroundSize="200px, cover"
+        backgroundBlendMode="soft-light"
+        p={3}
       >
+        <Stack isInline spacing={2} h={5} mb={2}>
+          {formattedTypes
+            ? formattedTypes.map((type) => (
+                <PokemonType key={type} type={type} />
+              ))
+            : new Array(2)
+                .fill(true)
+                .map((_, index) => (
+                  <Skeleton
+                    key={`skeleton-${index}`}
+                    boxShadow="md"
+                    startColor={mainColor}
+                    endColor={chroma(mainColor).brighten().css()}
+                    width="48px"
+                  />
+                ))}
+        </Stack>
+        <Skeleton
+          isLoaded={!!id}
+          startColor={mainColor}
+          endColor={chroma(mainColor).brighten().css()}
+        >
+          <Flex
+            alignItems="center"
+            h="72px"
+            color={chroma(mainColor).darken().hex()}
+            transition="color .4s"
+            _groupHover={{
+              color: chroma(mainColor).brighten(3).hex(),
+            }}
+          >
+            <Heading
+              as="h1"
+              size="lg"
+              maxH="60px"
+              overflow="hidden"
+              fontWeight="normal"
+              textTransform="capitalize"
+              transition="letter-spacing .4s"
+              cursor="unset"
+              _groupHover={{
+                letterSpacing: 'wide',
+              }}
+            >
+              {name}
+            </Heading>
+            <Heading as="h3" size="2xl" ml={2} cursor="unset">
+              #{number}
+            </Heading>
+          </Flex>
+        </Skeleton>
         <Box
           position="absolute"
-          h="full"
-          w="full"
-          borderRadius={12}
-          backgroundImage={`url(${
-            sprites?.other?.['official-artwork']?.front_default
-          }), linear-gradient(150deg, ${chroma(avgColor)
-            .brighten(0.8)
-            .css()}, ${mainColor})`}
-          backgroundRepeat="no-repeat"
-          backgroundPosition="110% 30%,center"
-          backgroundSize="200px, cover"
-          backgroundBlendMode="soft-light"
-          p={3}
+          top={-8}
+          right={-10}
+          zIndex={10}
+          transition="transform .4s"
+          _groupHover={{ transform: 'scale(1.1)' }}
         >
-          <Stack isInline spacing={2} h={5} mb={2}>
-            {formattedTypes
-              ? formattedTypes.map((type) => (
-                  <PokemonType key={type} type={type} />
-                ))
-              : new Array(2)
-                  .fill(true)
-                  .map((_, index) => (
-                    <Skeleton
-                      key={`skeleton-${index}`}
-                      boxShadow="md"
-                      startColor={mainColor}
-                      endColor={chroma(mainColor).brighten().css()}
-                      width={`${random(40, 60)}px`}
-                    />
-                  ))}
-          </Stack>
-          <Skeleton
-            isLoaded={!!id}
-            startColor={mainColor}
-            endColor={chroma(mainColor).brighten().css()}
-            w="80%"
-          >
-            <Flex
-              alignItems="center"
-              color={chroma(mainColor).darken().hex()}
-              transition="color .4s"
-              _groupHover={{ color: chroma(mainColor).darken(3).hex() }}
-            >
-              <Heading
-                as="h1"
-                size="xl"
-                fontWeight="normal"
-                textTransform="capitalize"
-              >
-                {name}
-              </Heading>
-              <Heading as="h3" size="2xl" ml={2}>
-                #{number}
-              </Heading>
-            </Flex>
-          </Skeleton>
-          <Box
-            position="absolute"
-            top={-8}
-            right={-10}
-            zIndex={10}
-            transition="transform .4s"
-            _groupHover={{ transform: 'scale(1.1)' }}
-          >
-            {sprites?.front_default ? (
-              <Image
-                src={sprites?.front_default}
-                width={116}
-                height={116}
-                quality={100}
+          {sprites?.front_default && (
+            <Image
+              src={sprites?.front_default}
+              width={116}
+              height={116}
+              quality={100}
+            />
+          )}
+
+          {!id && (
+            <Flex boxSize={116} alignItems="center" justifyContent="center">
+              <Spinner
+                size="lg"
+                thickness="4px"
+                color={chroma(mainColor).brighten().css()}
               />
-            ) : (
-              <Flex boxSize={116} alignItems="center" justifyContent="center">
-                <Spinner
-                  size="lg"
-                  thickness="4px"
-                  color={chroma(mainColor).brighten().css()}
-                />
-              </Flex>
-            )}
-          </Box>
+            </Flex>
+          )}
         </Box>
       </Box>
     </Link>
