1830e3f57d1973387dca3462d0d678feb8738dd5
Diego Kfuri <kkfuri@gmail.com>
2021-09-02 16:33:32 -0300

    fix: adjust button path cursor

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,13 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=PT+Sans+Caption&display=swap" rel="stylesheet" />
-          <style global>{`
+          <style global={true}>{`
             *,
             body {
               cursor: url(./cursor.cur), auto;
+            }
+            button path {
+              cursor: pointer;
             }
           `}</style>
         </Head>
